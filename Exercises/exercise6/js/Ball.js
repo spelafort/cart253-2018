dc23a9c0f5708f1764f62fbfefb3d34f5e50e73b
spelafort <spelafort@gmail.com>
2018-10-25 16:43:26 -0400

    Ex.5 Fixed prototip

--- Exercises/exercise6/js/Ball.js
+++ Exercises/exercise6/js/Ball.js
@@ -62,7 +62,8 @@ Ball.prototype.display = function () {
 //
 // Check if this ball overlaps the paddle passed as an argument
 // and if so reverse x velocity to bounce
-Ball.prototyp.handleCollision = function((paddle) {
+//FIXED 3
+Ball.prototype.handleCollision = function((paddle) {
   // Check if the ball overlaps the paddle on x axis
   if (this.x + this.size > paddle.x && this.x < paddle.x + paddle.w) {
     // Check if the ball overlaps the paddle on y axis
