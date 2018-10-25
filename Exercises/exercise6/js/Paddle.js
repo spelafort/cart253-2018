fdc6758b94af89d204b89b96ce4c40bd704f4e4f
spelafort <spelafort@gmail.com>
2018-10-25 16:46:21 -0400

    Ex.6 Fixed Prototypo

--- Exercises/exercise6/js/Paddle.js
+++ Exercises/exercise6/js/Paddle.js
@@ -23,7 +23,8 @@ function Pladdle(x,y,w,h,speed,downKey,upKey) {
 //
 // Check if the up or down keys are pressed and update velocity
 // appropriately
-Paddle.proto.handleInput = function() {
+//FIXED 7
+Paddle.prototype.handleInput = function() {
   if (keyDown(upKey)) {
     this.vy = -this.speed;
   }
@@ -43,7 +44,7 @@ Paddle.prototype.update = function() {
 // display()
 //
 // Draw the paddle as a rectangle on the screen
-//Fixed 6
+//FIXED 6
 Paddle.prototype.disploy = function() {
   rectangle(this.x,this.y,this.w,this.h);
 }
