797150d7950c92354647e267d55d850e74276ef4
spelafort <spelafort@gmail.com>
2018-10-25 16:50:43 -0400

    Ex.6 Fixed speeeed

--- Exercises/exercise6/js/Paddle.js
+++ Exercises/exercise6/js/Paddle.js
@@ -15,7 +15,8 @@ function Paddle(x,y,w,h,speed,downKey,upKey) {
   this.yv = 0;
   this.w = w;
   this.h = h;
-  this.speed = speeed;
+  //FIXED 12
+  this.speed = speed;
   this.downKey = downKey;
   this.upKey = upKey;
 }
