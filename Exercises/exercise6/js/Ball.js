5790e87dd8894aaca1cc5cc405919c547675d0ff
spelafort <spelafort@gmail.com>
2018-10-25 17:20:48 -0400

    Ex.6 Made ball white

--- Exercises/exercise6/js/Ball.js
+++ Exercises/exercise6/js/Ball.js
@@ -56,6 +56,8 @@ Ball.prototype.isOffScreen = function () {
 //
 // Draw the ball as a rectangle on the screen
 Ball.prototype.display = function () {
+  //FIXED 28
+  fill(255);
   rect(this.x, this.y);
 }
 
