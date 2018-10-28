2337fcd7ae8282616bfac8165275515fb409f5bb
spelafort <spelafort@gmail.com>
2018-10-28 07:45:14 -0400

    Ex.6 Fixed ball x value not updating

--- Exercises/exercise6/js/Ball.js
+++ Exercises/exercise6/js/Ball.js
@@ -25,7 +25,8 @@ function Ball(x,y,vx,vy,size,speed) {
 //FIXED 16
 Ball.prototype.update = function () {
   // Update position with velocity
-  this.x = this.vx;
+  //FIXED 30
+  this.x += this.vx;
   this.y += this.vy;
 
   // Constrain y position to be on screen
