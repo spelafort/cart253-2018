3a6adedf8ecda39c483a3e00cf97efb5a4a047b8
spelafort <spelafort@gmail.com>
2018-11-17 17:51:53 -0500

    Ex. 7 Made grid
    Made a scaling grid that randomly chooses a color from a 2 dimensional array for each grid point

---
+++ Exercises/exercise7/js/Point.js
@@ -0,0 +1,10 @@
+//defines the 'point' class of objects
+
+// Point
+//
+// Sets the properties with the provided arguments or defaults
+function Paddle(x,y) {
+  this.x = x;
+  this.y = y;
+  this.active = true;
+};
--- Exercises/exercise7/js/script.js
+++ Exercises/exercise7/js/script.js
@@ -1,27 +1,67 @@
-
-var angleX = 0.0;
-var angleY = 0.0;
+var startX = 50;
+var startY = 50;
+var pointDistance = 50;
+var numPointsRow;
+var numPointsColumn;
+var colorArray = [];
+var arrayOfPoints = [];
 
 function setup() {
-  createCanvas(500,500,WEBGL);
-  background(26,208,214);
+	createCanvas(800, 600);
+	background(92,88,76);
+	noStroke();
+	numPointsRow = width/pointDistance;
+	numPointsColumn = height/pointDistance;
+	putColors(94,57,41,colorArray);
+	console.log('color array index 0 is ' + colorArray[0])
+	putColors(183,209,163,colorArray);
+	console.log('color array index 1 is ' + colorArray[1])
+	putColors(228,113,106,colorArray);
+	console.log('color array index 2 is ' + colorArray[2])
+
+
+
 }
 
-function draw() {
-  noStroke();
-  rotateX(angleX);
-//  background(0);
-  fill(244,252,232);
-  rotateY(angleY);
-  box(60);
-  translate(50,0,50); // Translation in 3D!
-  rotateY(angleY*10);
-  fill(135,214,155);
-  box(30);
-  translate(25,0,25)
-  fill(78,150,137);
-  rotateY(angleY*100);
-  box(15);
-  angleX += 0.01;
-  angleY -= 0.01;
+function draw(){
+	//draw rows on the basis of screen height
+	while(startY < height) {
+	drawGridRow(startY);
+	startY = startY + pointDistance;
+}
+}
+
+//function to draw a row on the basis of screen width
+function drawGridRow(startY){
+	fill(setFill(true,false,false,colorArray),setFill(false,true,false,colorArray),setFill(false,false,true,colorArray));
+	var pointsDrawn = 0;
+  var x = startX;
+  while (pointsDrawn < numPointsRow-1) {
+		f
+		fill(setFill(true,false,false,colorArray),setFill(false,true,false,colorArray),setFill(false,false,true,colorArray));
+    ellipse(x,startY,pointDistance);
+    x += pointDistance;
+    pointsDrawn++;
+	}
+}
+
+//function that will populate an array with some super cool colors
+function putColors(r,g,b,colorArray){
+var color = [r,g,b];
+colorArray.push(color);
+}
+
+//function that randomly picks a color and determines fill for a specific row, making sure not to mix and match RGB values
+function setFill(r,g,b, colorArray){
+	//first, choose a color of the three possibilities at random, returning either 0, 1, or 2
+firstOrder = Math.floor(random(1,3))
+	console.log('first order is ' + firstOrder);
+
+	if(r === true){
+		return colorArray[firstOrder][0];
+	}else if(g === true){
+		return colorArray[firstOrder][1];
+	}else if(b === true){
+		return colorArray[firstOrder][2];
+	}
 }
